21f2af9051b858eb3f0d59ae8bd335f8574b63cf
Matheus Freitas <mgsoutodelira@gmail.com>
2024-09-21 00:01:28 -0300

    update

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
 const form = document.getElementById("form");
-const button = document.getElementById("button");
 const text = document.getElementById("text");
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
+    checkForm();
+})
+
+text.addEventListener(("blur"), () => {
     checkInputEmail();
 })
 
 function checkInputEmail() {
     const emailValue = text.value;
+    const formItem = text.parentElement;
+
     
     if (emailValue === "") {
         errorInput(text, "Digite um email válido.");
+    } else if(!emailValue.includes("@") || !emailValue.includes(".com")){
+        errorInput(text, "Digite um email válido.");
+    } else {
+        formItem.className = "form-content ok";
+        return true;
     }
-
-    console.log(emailValue)
 }
 
 function errorInput(element, message){
     const formItem = element.parentElement;
-    const textMessage = formItem.querySelector("span")
+    const textMessage = formItem.querySelector("span");
     
     textMessage.innerText = message;
     formItem.className = "form-content error";
 }
 
-// clicar(button);
-
-function clicar(elemento) {
-    elemento.addEventListener("click", () => {
-        alert("Enviado");
-        validarEmail(input);
-    })
+function checkForm() {
+    const isValid = checkInputEmail();
+    if (isValid) {
+        alert("Cadastrado com sucesso!")
+    } 
 }
+
